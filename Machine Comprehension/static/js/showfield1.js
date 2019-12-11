cb65ad32af750efc897607e6b0b9d18b80acb8c1
Kedar Dabhadkar <dkedar@cmu.edu>
2019-12-10 20:09:44 -0500

    Update index template

---
+++ Machine Comprehension/static/js/showfield1.js
@@ -0,0 +1,4 @@
+function showfield1(name){
+if(name=="revel")document.getElementById('textarea1').innerHTML='Revel is a full-service digital consultancy that partners with organizations to lead go-to-market transformation by building seamless, simple, connected experiences. We are problem solvers, designers and builders united around a core set of values and a commitment to solving complex challenges swiftly, efficiently and elegantly.';
+else document.getElementById('div1').innerHTML='';
+}
---
+++ Machine Comprehension/static/js/showfield2.js
@@ -0,0 +1,6 @@
+function showfield2(name){
+    if(name=="revelQ1")document.getElementById('textarea2').innerHTML="What is Revel?";
+    else if (name=="revelQ2")document.getElementById('textarea2').innerHTML="What does Revel do?";
+    else if (name=="revelQ3")document.getElementById('textarea2').innerHTML="How does Revel solve challenges?";
+    else document.getElementById('div1').innerHTML='';
+        }
